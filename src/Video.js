97a1bd4c3e53f3ecd046374720455bbc5355fdd0
amaurs <amaury.gtz@gmail.com>
2018-01-10 00:10:38 -0600

    Component to handle video.

---
+++ src/Video.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+import './Video.css';
+
+class Video extends Component {
+    render() {
+        let headers = this.props.info.info.headers.map((h, index) => <h3 key={index}>{h}</h3>);
+        let paragraphs = this.props.info.info.paragraphs.map((p, index) => <p key={index}>{p}</p>);
+        
+        let style = {
+          'padding-top': this.props.info.info.aspect + "%"
+        }
+
+        return  <div className="Video-container">
+                   <div className="Video-player-containter">
+                      <div className="Video-wrapper" style={style}>
+                        <ReactPlayer 
+                          className="Video-player"
+                          width='100%'
+                          height='100%'
+                          url={this.props.info.info.url}
+                          playing
+                          muted
+                          loop/>
+                      </div>
+                   </div>
+                   <div className="Video-text-container">
+                        <div className="Video-text">
+                          <h1>{this.props.info.info.title}</h1>
+                          {headers}
+                          {paragraphs}
+                        </div>
+                   </div>
+                </div>
+    }
+
+}
+
+export default Video;
