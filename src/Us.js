f04b866615b726d77ab21969b01049cc94d7eaa6
amaurs <amaury.gtz@gmail.com>
2018-09-11 17:24:48 -0500

    Added some texts in french.

--- src/Us.js
+++ src/Us.js
@@ -5,12 +5,19 @@ export default class Us extends Component {
     render() {
       return ( 
         <div className="Us">
+          <p>{this.props.t("about-paragraph0")}</p>
           <p>{this.props.t("about-paragraph1")}</p>
           <p>{this.props.t("about-paragraph2")}</p>
           <p>{this.props.t("about-paragraph3")}</p>
           <p>{this.props.t("about-paragraph4")}</p>
           <p>{this.props.t("about-paragraph5")}</p>
           <p>{this.props.t("about-paragraph6")}</p>
+          <p>{this.props.t("about-paragraph7")}</p>
+          <p>{this.props.t("about-paragraph8")}</p>
+          <p>{this.props.t("about-paragraph9")}</p>
+          <p>{this.props.t("about-paragraph10")}</p>
+          <p>{this.props.t("about-paragraph11")}</p>
+          <p>{this.props.t("about-paragraph12")}</p>
         </div>
       )
     }
